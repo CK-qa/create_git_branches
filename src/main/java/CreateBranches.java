090b04c99782de8344d4492b81ddcb561b6a3746
Vika <victoria.bozhko@gmail.com>
2021-04-16 17:00:49 +0300

    Initial commit

---
+++ src/main/java/CreateBranches.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class CreateBranches {
+
+    public void create() throws IOException {
+        Process p = Runtime.getRuntime().exec("open -n -F -a /Applications/Utilities/Terminal.app --args ls");
+
+    }
+}
